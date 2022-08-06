3aa17f27691adb25dbded048f27040c514d4bac6
Eduardo Batista <dudubatista@gmail.com>
2022-08-06 01:02:10 -0300

    Changing to build with npm and nunjucks.

---
+++ myscripts.js
@@ -0,0 +1,13 @@
+const njk = require('nunjucks')
+const fs = require('fs');
+
+module.exports.buildfiles = function () {
+    njk.configure("src",{autoscape: true});
+    const filesToRender = ['index.html', 'projects.html', 'teaching.html', 'research.html'];
+    filesToRender.forEach( function(fname,i) {
+      fs.writeFile(fname,njk.render(fname), function (err) {
+        if (err) return console.log(err);
+        console.log('Done:' + fname);
+      });
+    });
+}
