a72c5a4dd19df0b1d4a8160d126a99671e9f9cf5
Eduardo Batista <dudubatista@gmail.com>
2022-08-07 00:47:47 -0300

    First reasonable deployment.

--- myscripts.js
+++ myscripts.js
@@ -5,7 +5,7 @@ module.exports.buildfiles = function () {
     njk.configure("src",{autoscape: true});
     const filesToRender = ['index.html', 'projects.html', 'teaching.html', 'research.html'];
     filesToRender.forEach( function(fname,i) {
-      fs.writeFile(fname,njk.render(fname), function (err) {
+      fs.writeFile("docs/" + fname,njk.render(fname), function (err) {
         if (err) return console.log(err);
         console.log('Done:' + fname);
       });
